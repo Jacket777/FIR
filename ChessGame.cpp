1333c6abe34464d6411787fa9335075adec1cbdd
Jacket777 <hxqsu37@sina.com>
2023-05-02 16:40:36 +0800

    初始游戏接口

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -1,5 +1,33 @@
 #include "ChessGame.h"
 
+ChessGame::ChessGame(Man* man, AI* ai, Chess* chess)
+{
+	this->man = man;
+	this->ai = ai;
+	this->chess = chess;
+
+}
+
+//对局 开始游戏
 void ChessGame::play()
 {
+	while (1) {
+		chess->init();
+
+
+
+		//先由棋手下棋
+		man->go();
+		if (chess->checkOver()) {
+			chess->init();
+			continue;
+		}
+		//AI 下棋
+		ai->go();
+		if (chess->checkOver()) {
+			chess->init();
+			continue;
+		}
+	}
+
 }
